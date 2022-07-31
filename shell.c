c7c024dd783938425cca2ce4e0b5740c39a50684
Trevor <chelimotrevorkibowen@gmail.com>
2022-08-01 00:56:30 +0300

    my first commit

---
+++ shell.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+
+int print_start()
+{
+    printf("$");
+}
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+
+int main()
+{
+    print_start();
+
+    return 0;
+}
