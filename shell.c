a024ff5bc64d9391fa7fe27c9d7fe25d56f7b150
Kibowen <Kibowen>
2022-08-03 03:56:21 -0700

    promtp works.

--- shell.c
+++ shell.c
@@ -4,11 +4,11 @@
 int main(void){
     char *buffer = NULL;
     // ssize_t num_read;
-    size_t n = 128;
+    size_t n = 0;
 
     printf("$ ");
 
-    fgets(buffer, n, stdin);
+	getline(&buffer, &n, stdin);
 
     printf(">>>>>> %s\n", buffer);
     // printf("%ld \n",  num_read);
