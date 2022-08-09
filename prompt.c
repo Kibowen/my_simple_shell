e72171affcfd5921a158b5b0d7026324ae0a98e4
Trevor <chelimotrevorkibowen@gmail.com>
2022-08-09 23:35:45 +0300

    removed new screen prompt

--- prompt.c
+++ prompt.c
@@ -3,14 +3,6 @@
 int print_prompt(void)
 {
 
-    static int first_time = 1;
-    if (first_time)
-    {
-        const char* CLEAR_SCREEN_ANSI = " \e[1;1H\e[2j";
-        write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
-        first_time = 0;
-    }
-
     printf("$ ");
 
     return 0;
